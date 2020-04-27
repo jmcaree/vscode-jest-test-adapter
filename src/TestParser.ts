a6a95381f7f19c5bc950bc5e1ee901e4809013dd
Ross Knudsen <rossk@datacom.co.nz>
2020-04-27 13:40:24 +1200

    fix: Added error handling to TestParser.  Returns results for successfully parsed test files.

--- src/TestParser.ts
+++ src/TestParser.ts
@@ -1,5 +1,11 @@
-import * as fs from "fs";
-import { IParseResults, JestSettings, parse } from "jest-editor-support";
+import fs from "fs";
+import {
+  IParseResults,
+  JestSettings,
+  parse as editorSupportParse,
+  ParsedNode,
+  ParsedNodeTypes,
+} from "jest-editor-support";
 import _ from "lodash";
 import * as mm from "micromatch";
 import * as path from "path";
@@ -12,121 +18,6 @@ import { cancellationTokenNone, Matcher } from "./types";
  * Only universally recognized patterns should be used here, such as node_modules.
  */
 const IGNORE_GLOBS = ["node_modules"];
-
-/**
- * Returns true if the specified path is a directory, false otherwise.
- * @param directory The full file system path to the check.
- */
-function checkIsDirectory(
-  directory: string,
-  cancellationToken: vscode.CancellationToken = cancellationTokenNone,
-): Promise<boolean> {
-  let reject: (reason?: any) => void;
-
-  const promise = new Promise<boolean>((resolve, xReject) => {
-    reject = xReject;
-
-    fs.stat(directory, (err, stats) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(stats.isDirectory());
-      }
-    });
-  });
-
-  cancellationToken.onCancellationRequested(() => reject("Cancellation requested."));
-
-  return promise;
-}
-
-/**
- * Creates a matcher function that returns true if a file should be explored for tests, false otherwise.
- * @param settings The Jest settings.
- */
-function createMatcher(settings: JestSettings): Matcher {
-  // TODO what to do if there is more than one config?...
-
-  if (settings?.configs?.length > 0 && settings.configs[0].testRegex?.length > 0) {
-    const regex = new RegExp(settings.configs[0].testRegex[0]);
-    return value => regex.test(value);
-  } else {
-    return value => mm.any(value, settings.configs[0].testMatch);
-  }
-}
-
-/**
- * Explores a directory recursively and returns the TestSuiteInfo representing the directory.
- * @param directory The full file system path to the directory.
- * @param matcher The matcher function to use to determine if a file includes tests.
- */
-async function exploreDirectory(
-  directory: string,
-  matcher: Matcher,
-  cancellationToken: vscode.CancellationToken = cancellationTokenNone,
-): Promise<IParseResults[]> {
-  const contents = await getDirectoryContents(directory, cancellationToken);
-  const files = await Promise.all(contents.map(x => evaluateFilePath(x, matcher, cancellationToken)));
-  return _.flatten(files);
-}
-
-/**
- * Evaluates a file path and returns the TestSuiteInfo representing it.
- * If the path is a directory, it will recursively explore it.
- * If the path is a file, it will parse the contents and search for test blocks.
- * If the directory or file did not include any tests, null will be returned.
- * @param filePath The file path to evaluate.
- * @param matcher The matcher function to use to determine if a file includes tests.
- */
-async function evaluateFilePath(
-  filePath: string,
-  matcher: Matcher,
-  cancellationToken: vscode.CancellationToken = cancellationTokenNone,
-): Promise<IParseResults[]> {
-  const isDirectory = await checkIsDirectory(filePath, cancellationToken);
-  if (isDirectory) {
-    return await exploreDirectory(filePath, matcher, cancellationToken);
-  } else if (matcher(filePath)) {
-    return [parse(filePath)];
-  } else {
-    return [];
-  }
-}
-
-/**
- * Retrieves the contents of a directory and outputs their absolute paths.
- * Includes both files and directories.
- * Excludes glob patterns included in IGNORE_GLOBS.
- * @param directory Returns an array of absolute paths representing the items within the directory.
- */
-function getDirectoryContents(
-  directory: string,
-  cancellationToken: vscode.CancellationToken = cancellationTokenNone,
-): Promise<string[]> {
-  let reject: (reason?: any) => void;
-
-  const promise = new Promise<string[]>((resolve, xReject) => {
-    reject = xReject;
-
-    fs.readdir(directory, (err, files) => {
-      if (err) {
-        reject(err);
-      } else {
-        const includedFiles = mm.not(files, IGNORE_GLOBS);
-        resolve(includedFiles.map(f => path.join(directory, f)));
-      }
-    });
-  });
-
-  cancellationToken.onCancellationRequested(() => reject("Cancellation requested."));
-  return promise;
-}
-
-// interface Cancellable {
-//   cancel: () => void;
-// }
-
-// interface CancellablePromise<T> extends PromiseLike<T>, Cancellable {}
 
 class TestParser {
   public constructor(
@@ -143,23 +34,156 @@ class TestParser {
     // TODO there is a Jest CLI option --listTests that will provide a list of test files that the current Jest config
     // resolves.  We should use this instead of trying to do the regex ourselves.
 
-    const parsedResults = await exploreDirectory(this.rootPath, matcher, cancellationToken);
+    const parsedResults = await this.exploreDirectory(this.rootPath, matcher, cancellationToken);
     this.log.info("Test load complete");
 
     return parsedResults;
   }
 
-  public parseFiles(files: string[], cancellationToken: vscode.CancellationToken = cancellationTokenNone): IParseResults[] {
+  public parseFiles(
+    files: string[],
+    cancellationToken: vscode.CancellationToken = cancellationTokenNone,
+  ): IParseResults[] {
     // TODO this method should potentially be async...
     const matcher = createMatcher(this.settings);
-    
-    return files.filter(matcher).map(f => {
+
+    return files.filter(matcher).map((f) => {
       if (cancellationToken.isCancellationRequested) {
         throw Error("Cancellation requested.");
       }
-      return parse(f);
+      return this.parse(f);
     });
   }
+
+  /**
+   * Evaluates a file path and returns the TestSuiteInfo representing it.
+   * If the path is a directory, it will recursively explore it.
+   * If the path is a file, it will parse the contents and search for test blocks.
+   * If the directory or file did not include any tests, null will be returned.
+   * @param filePath The file path to evaluate.
+   * @param matcher The matcher function to use to determine if a file includes tests.
+   */
+  private async evaluateFilePath(
+    filePath: string,
+    matcher: Matcher,
+    cancellationToken: vscode.CancellationToken = cancellationTokenNone,
+  ): Promise<IParseResults[]> {
+    const isDirectory = await this.checkIsDirectory(filePath, cancellationToken);
+    if (isDirectory) {
+      return await this.exploreDirectory(filePath, matcher, cancellationToken);
+    } else if (matcher(filePath)) {
+      return [this.parse(filePath)];
+    } else {
+      return [];
+    }
+  }
+
+  /**
+   * Returns true if the specified path is a directory, false otherwise.
+   * @param directory The full file system path to the check.
+   */
+  private checkIsDirectory(
+    directory: string,
+    cancellationToken: vscode.CancellationToken = cancellationTokenNone,
+  ): Promise<boolean> {
+    let reject: (reason?: any) => void;
+
+    const promise = new Promise<boolean>((resolve, xReject) => {
+      reject = xReject;
+
+      fs.stat(directory, (err, stats) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(stats.isDirectory());
+        }
+      });
+    });
+
+    cancellationToken.onCancellationRequested(() => reject("Cancellation requested."));
+
+    return promise;
+  }
+
+  /**
+   * Explores a directory recursively and returns the TestSuiteInfo representing the directory.
+   * @param directory The full file system path to the directory.
+   * @param matcher The matcher function to use to determine if a file includes tests.
+   */
+  private async exploreDirectory(
+    directory: string,
+    matcher: Matcher,
+    cancellationToken: vscode.CancellationToken = cancellationTokenNone,
+  ): Promise<IParseResults[]> {
+    const contents = await this.getDirectoryContents(directory, cancellationToken);
+    const files = await Promise.all(contents.map((x) => this.evaluateFilePath(x, matcher, cancellationToken)));
+    return _.flatten(files);
+  }
+
+  /**
+   * Parses the given test file using `jest-editor-support`'s `parse` method.  If there are parsing errors, will return
+   * a placeholder result with empty tests.  In future, this will return an error object to display the results to the
+   * user.
+   * @param file the path of the file to parse.
+   */
+  private parse(file: string): IParseResults {
+    try {
+      return editorSupportParse(file);
+    } catch (error) {
+      this.log.error(error);
+      return {
+        describeBlocks: [],
+        expects: [],
+        file,
+        itBlocks: [],
+        root: new ParsedNode(ParsedNodeTypes.root, file),
+      };
+    }
+  }
+
+  /**
+   * Retrieves the contents of a directory and outputs their absolute paths.
+   * Includes both files and directories.
+   * Excludes glob patterns included in IGNORE_GLOBS.
+   * @param directory Returns an array of absolute paths representing the items within the directory.
+   */
+  private getDirectoryContents(
+    directory: string,
+    cancellationToken: vscode.CancellationToken = cancellationTokenNone,
+  ): Promise<string[]> {
+    let reject: (reason?: any) => void;
+
+    const promise = new Promise<string[]>((resolve, xReject) => {
+      reject = xReject;
+
+      fs.readdir(directory, (err, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          const includedFiles = mm.not(files, IGNORE_GLOBS);
+          resolve(includedFiles.map((f) => path.join(directory, f)));
+        }
+      });
+    });
+
+    cancellationToken.onCancellationRequested(() => reject("Cancellation requested."));
+    return promise;
+  }
 }
+
+/**
+ * Creates a matcher function that returns true if a file should be explored for tests, false otherwise.
+ * @param settings The Jest settings.
+ */
+const createMatcher = (settings: JestSettings): Matcher => {
+  // TODO what to do if there is more than one config?...
+
+  if (settings?.configs?.length > 0 && settings.configs[0].testRegex?.length > 0) {
+    const regex = new RegExp(settings.configs[0].testRegex[0]);
+    return (value) => regex.test(value);
+  } else {
+    return (value) => mm.any(value, settings.configs[0].testMatch);
+  }
+};
 
 export { TestParser as default, createMatcher };
